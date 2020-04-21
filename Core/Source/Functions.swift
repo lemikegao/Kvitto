fbf2c740e1d1fdbfb74fa7d872e2cb272d922e1b
Michael Gao <mgao.ucla@gmail.com>
2020-04-21 07:25:23 -0700

    Return Bool? for _boolFromData func

--- Core/Source/Functions.swift
+++ Core/Source/Functions.swift
@@ -63,7 +63,7 @@ internal func _optionalStringFromData(_ data: Data) throws -> String?
     return string
 }
 
-internal func _boolFromData(_ data: Data) -> Date?
+internal func _boolFromData(_ data: Data) -> Bool?
 {
     guard let string = try _stringFromData(data)
     else {
